9980f949dd0041113c219101a99dc2a51f9db058
thequlmatov <zuxroka0104@gmail.com>
2023-12-12 17:13:39 +0500

    xabar

---
+++ main.js
@@ -0,0 +1,18 @@
+// 1 => faqat account ochganda yoziladi
+// git config --global user.name "Foydalanuvchi nomi"
+// git config --global user.email "Foydalanuvchi pochtasi"
+
+// 2 => faqat repository ochganda
+// git init
+
+
+// 3 =>origin nomli "folder" => faqat repeostory yaratilganda
+// git remote add origin "path"
+
+// 4 => fayllarni cloud yuklaydi
+// git .
+
+// 5 => Comment
+// git commit -m "xabar"
+
+// 6 => git push origin master
